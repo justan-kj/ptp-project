4fba342daebe5c5f0b87be0fceac0fda91dbeadb
Justin Tan <jtjtjt703@gmail.com>
2024-11-22 16:36:15 +0000

    created Area class

---
+++ dungeon_game/area.py
@@ -0,0 +1,13 @@
+
+class Area:
+    """The area class represents a single space the player move through. It has exits determining valid movement options"""
+    def __init__(self):
+        """Initializes the area. It is a closed room by default
+        :return: None
+        """
+        self.exits = {
+            'N':False,
+            'E':False,
+            'S':False,
+            'W':False
+        }
