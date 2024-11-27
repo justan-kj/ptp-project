c820263fffe0fd1f8a8580df02d979c1bb43c77c
Justin Tan <jtjtjt703@gmail.com>
2024-11-27 17:29:17 +0000

    added basic script to initialise a new game

---
+++ dungeon_game/main.py
@@ -0,0 +1,9 @@
+from dungeon_game.game import Game
+
+def main():
+    player_name = input("Welcome to Adventure world, please enter your character's name: ")
+    new_game = Game(player_name)
+    new_game.start()
+
+if __name__ == '__main__':
+    main()
