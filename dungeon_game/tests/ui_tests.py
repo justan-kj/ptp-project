558ba441a82a6d0c1bc40b354b708c43d4a104b8
Justin Tan <jtjtjt703@gmail.com>
2024-11-27 14:06:20 +0000

    added documentation to ui class and test

--- dungeon_game/tests/ui_tests.py
+++ dungeon_game/tests/ui_tests.py
@@ -3,13 +3,22 @@ from dungeon_game.ui import UserInterface
 
 class TestUi(unittest.TestCase):
     def setUp(self):
+        """Sets up a test ui
+        :return: None
+        """
         self.ui = UserInterface()
         pass
 
     def testInit(self):
+        """Checks that initialised values are as expected
+        :return: None
+        """
         self.assertEqual(self.ui.log, [])
 
     def testUpdate(self):
+        """Checks that messages are added to the log when update is called
+        :return: None
+        """
         self.ui.log = []
         self.ui.update("Test message 1")
         self.assertEqual(len(self.ui.log), 1)
--- dungeon_game/ui.py
+++ dungeon_game/ui.py
@@ -1,8 +1,18 @@
 class UserInterface:
+    """The UserInterface class stores and presents the text prompts the player
+     interacts with"""
     def __init__(self):
+        """Initializes the ui. Log contains all the messages that have been displayed thus far
+        :return: None
+        """
         self.log = []
 
     def update(self, message):
+        """
+        Prints a message to the console then saves it into the log
+        :param message: The message to be displayed and saved
+        :return:
+        """
         self.log.append(message)
         print(message)
         pass
