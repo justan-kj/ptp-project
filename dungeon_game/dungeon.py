16604623f1e6407b18e53f7eb0fc1e8b5b974ccf
Justin Tan <jtjtjt703@gmail.com>
2024-11-27 17:25:29 +0000

    added method to generate a path through areas. Simple implementation: drills a straight line from west to east from the starting point, removing the East wall of the current area and west wall of the next.

--- dungeon_game/dungeon.py
+++ dungeon_game/dungeon.py
@@ -1,4 +1,10 @@
+
 from dungeon_game.area import Area
+
+def bridge_areas(area1,area2):
+    area1.exits["E"] = True
+    area2.exits["W"] = True
+    return area2
 
 class Dungeon():
     """The Dungeon class represents the space the player passes through.
@@ -8,11 +14,16 @@ class Dungeon():
         :param size: a 2-tuple of the form (rows, columns)
         :return: None
         """
+        self.size = size
         rows, cols = size
         self.areas = [[Area() for _ in range(cols)] for _ in range(rows)]
 
-
-
-
-
+    def generate_path(self, starting_point):
+        start_row,start_col = starting_point
+        rows, cols = self.size
+        current_col = start_col
+        while current_col < cols - 1:
+            bridge_areas(self.areas[start_row][current_col], self.areas[start_row][current_col + 1])
+            current_col += 1
+        return self.areas[start_row][current_col]
 
