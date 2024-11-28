375fb35a8ba151585787f3c1403a45708bb1a317
justan-kj <jtjtjt703@gmail.com>
2024-11-28 22:38:50 +0000

    added basic movement functionality

--- dungeon_game/tests/dungeon_tests.py
+++ dungeon_game/tests/dungeon_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from dungeon_game.dungeon import Dungeon
+from dungeon_game.dungeon import *
 from dungeon_game.area import Area
 
 def all_items_are_type(a_2d_list,a_type):
@@ -43,3 +43,51 @@ class TestDungeon(unittest.TestCase):
                 self.assertTrue(self.dungeon.areas[0][col].exits["W"])
         self.assertTrue(self.dungeon.areas[0][cols - 1].exits["W"])
 
+    def test_get_area(self):
+        """Checks that get_area returns the correct area
+        :return: None
+        """
+        area = self.dungeon.get_area(2, 3)
+        self.assertIsInstance(area, Area)
+        self.assertEqual(area, self.dungeon.areas[2][3])
+
+    def test_get_adjacent_areas(self):
+        """Checks that get_adjacent_areas returns correctly linked areas
+        :return: None
+        """
+        start_row, start_col = 0, 0
+        self.dungeon.generate_path((start_row, start_col))
+        adjacent = self.dungeon.get_adjacent_areas(start_row, start_col)
+
+        # The East area should be valid
+        self.assertIsInstance(adjacent["E"], Area)
+
+        # The other directions should not be connected yet
+        self.assertFalse(adjacent["N"])
+        self.assertFalse(adjacent["S"])
+        self.assertFalse(adjacent["W"])
+
+    def test_bridge_areas(self):
+        """Checks that bridge_areas correctly links two areas
+        :return: None
+        """
+        area1 = self.dungeon.areas[0][0]
+        area2 = self.dungeon.areas[0][1]
+        bridge_areas(area1, area2)
+        self.assertTrue(area1.exits["E"])
+        self.assertTrue(area2.exits["W"])
+
+    def test_check_path(self):
+        """Checks that check_path correctly identifies linked areas
+        :return: None
+        """
+        area1 = self.dungeon.areas[0][0]
+        area2 = self.dungeon.areas[0][1]
+        area3 = self.dungeon.areas[1][3]
+
+        # Link area1 and area2
+        bridge_areas(area1, area2)
+
+        # Validate paths
+        self.assertTrue(check_path(area1, area2))
+        self.assertFalse(check_path(area2, area3))
