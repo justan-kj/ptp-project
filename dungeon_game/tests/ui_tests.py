6a3b15ea11d0b09da6e644014dd8830ef087ae90
Justin Tan <jtjtjt703@gmail.com>
2024-11-28 19:08:58 +0000

    added a choice selection function to ui

--- dungeon_game/tests/ui_tests.py
+++ dungeon_game/tests/ui_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from dungeon_game.ui import UserInterface
+from dungeon_game.ui import UserInterface, Choice
 
 class TestUi(unittest.TestCase):
     def setUp(self):
@@ -26,3 +26,11 @@ class TestUi(unittest.TestCase):
         self.ui.update("Test message 2")
         self.assertEqual(len(self.ui.log), 2)
         self.assertEqual(self.ui.log[1], "Test message 2")
+
+    def testChoices(self):
+        self.ui.log = []
+        choices = [Choice("Test choice 1", "val2"),
+                   Choice("Test choice 2", "val2"),
+                   Choice("Test choice 3", "val2")]
+        self.assertEqual(self.ui.get_choice(choices,1),"val2")
+        self.assertEqual(self.ui.log[-1], "Test choice 2")
--- dungeon_game/ui.py
+++ dungeon_game/ui.py
@@ -1,3 +1,12 @@
+class Choice:
+    """
+    The Choice class is a simple container containing a string as a text prompt and a value representing the outcome of the choice
+    """
+    def __init__(self, prompt,value):
+        self.prompt = prompt
+        self.value = value
+
+
 class UserInterface:
     """The UserInterface class stores and presents the text prompts the player
      interacts with"""
@@ -15,5 +24,32 @@ class UserInterface:
         """
         self.log.append(message)
         print(message)
-        pass
+
+    def get_choice(self, choices, selection=None):
+        """
+        Prints a list of choices sequentially and prompts a player to select one of them
+        returning the Choice's value if the selection is succesful
+        :param choices: The choices to be selected from
+        :return: The value of the selected Choice
+        """
+        choice_num = 1
+        for choice in choices:
+            print(f"{choice_num}) {choice.prompt}")
+            self.log.append(choice.prompt)
+            choice_num+=1
+
+        if not selection is None:
+            self.log.append(choices[selection].prompt)
+            return choices[selection].value
+        while True:
+            player_selection = input("Select a choice: ")
+            try:
+                if int(player_selection) - 1 in range(len(choices)):
+                    self.log.append(choices[player_selection].prompt)
+                    return choices[player_selection].value
+            except:
+                print("Invalid choice")
+
+
+
 
