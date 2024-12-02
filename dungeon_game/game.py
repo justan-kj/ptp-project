7d3fefe5b827dd0e5b29de69f8e375f1359c9ef5
Justin Tan <jtjtjt703@gmail.com>
2024-12-02 10:14:17 +0000

    shifted intialisation logic into new method, enabling game to restart

--- dungeon_game/game.py
+++ dungeon_game/game.py
@@ -4,24 +4,50 @@ from dungeon_game.direction import Position
 from dungeon_game.dungeon import Dungeon
 from dungeon_game.player import Player
 from dungeon_game.ui import UserInterface, Choice
+import os
 
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 def generate_seed():
     output = random.randint(0,2**32)
     return output
 
 class Game:
-    def __init__(self, name, size=(5,5), seed=generate_seed()):
-        self.seed = seed
+    def __init__(self, name=None):
+        self.dungeon = None
+        self.player = None
+        self.seed = 0
+        self.ui = None
+        self.initialize(name)
+
+    def initialize(self,player_name=None):
+        if player_name is None:
+            player_name = input("Welcome to Adventure world, please enter your character's name: ")
+
+        size = (5,5)
+        self.seed = generate_seed()
         self.dungeon = Dungeon(size)
-        starting_pos = Position(0,0,size[0],size[1])
-        self.player = Player(name, starting_pos)
+        starting_pos = Position(0, 0, size[0], size[1])
+        self.player = Player(player_name, starting_pos)
         self.ui = UserInterface()
 
     def start(self):
         self.ui.update(f"The hero {self.player.name} arrives in the dungeon, embarking in search of a legendary treasure.")
         while True:
             self.move_player()
+            if self.player.position.equals(self.dungeon.endpoint):
+
+                break
+    def end_game(self,victory = True):
+        if victory:
+            print("Congratulations! You beat the game.")
+            new_game_check = input("Play again? Y/N")
+            if new_game_check.upper() == "Y":
+                cls()
+                self.initialize()
+
+
 
     def move_player(self):
         open_paths = self.dungeon.get_adjacent_areas(self.player.position)
