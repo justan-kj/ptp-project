f716b01616e072bf5a515864c4f034531ac205ba
Justin Tan <jtjtjt703@gmail.com>
2024-12-03 23:14:56 +0000

    improved str representation of area visually represents exits available

--- dungeon_game/area.py
+++ dungeon_game/area.py
@@ -1,5 +1,37 @@
 from dungeon_game.direction import Direction
 
+#WNES
+char_map = {
+    "0000":" ",
+    "0001":"╷",
+    "0010":"╶",
+    "0011":"┌",
+    "0100": "╵",
+    "0101":"│",
+    "0110":"└",
+    "0111": "├",
+    "1000":"╴",
+    "1001":"┐",
+    "1010":"─",
+    "1011":"┬",
+    "1100":"┘",
+    "1101":"┤",
+    "1110":"┴",
+    "1111":"┼"
+}
+
+def get_symbol(exits):
+        value = 0
+        if exits[Direction.WEST]:
+            value += 1000
+        if exits[Direction.NORTH]:
+            value += 100
+        if exits[Direction.EAST]:
+            value += 10
+        if exits[Direction.SOUTH]:
+            value += 1
+        return f"{value:>04}"
+
 
 class Area:
     """The area class represents a single space the player move through. It has exits determining valid movement options"""
@@ -17,6 +49,7 @@ class Area:
         self.col = position.col
 
     def __repr__(self):
-        return f"R{self.row}C{self.col}"
+        code = get_symbol(self.exits)
+        return char_map[code]
 
 
