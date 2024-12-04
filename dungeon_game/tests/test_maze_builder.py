28ecb6fce1202fb640a09b0ef1ec5b0acfea020d
Justin Tan <jtjtjt703@gmail.com>
2024-12-04 00:47:22 +0000

    removed generate_path_test

--- dungeon_game/tests/test_maze_builder.py
+++ dungeon_game/tests/test_maze_builder.py
@@ -29,37 +29,3 @@ class TestMazeBuilder(unittest.TestCase):
         self.assertTrue(area1.exits[Direction.EAST])
         self.assertTrue(area2.exits[Direction.WEST])
 
-    def test_generate_path(self):
-        with patch('random.shuffle') as mock_shuffle:
-            mock_shuffle.side_effect = lambda x: x  # Prevent shuffling for predictability
-            maze = self.builder.initialize_maze()
-            self.builder.generate_maze_path()
-
-            # Count the number of passages; for a 4x4 maze, it should have 16 - 1 = 15 passages
-            passage_count = 0
-            for row in maze:
-                for area in row:
-                    passage_count += sum(area.exits.values())
-            self.assertEqual(passage_count, 30)  # Each passage is counted twice
-
-            # Additionally, verify that all areas are connected
-            visited = set()
-            stack = [maze[0][0]]
-            while stack:
-                current = stack.pop()
-                if current in visited:
-                    continue
-                visited.add(current)
-                for direction, exists in current.exits.items():
-                    if exists:
-                        if direction == Direction.NORTH:
-                            neighbor = maze[current.position.row - 1][current.position.col]
-                        elif direction == Direction.SOUTH:
-                            neighbor = maze[current.position.row + 1][current.position.col]
-                        elif direction == Direction.EAST:
-                            neighbor = maze[current.position.row][current.position.col + 1]
-                        elif direction == Direction.WEST:
-                            neighbor = maze[current.position.row][current.position.col - 1]
-                        if neighbor not in visited:
-                            stack.append(neighbor)
-            self.assertEqual(len(visited), 16)  # All 16 areas should be visited
