8b7c3612b688c47b4990f06a476f3b8868aa166c
Justin Tan <jtjtjt703@gmail.com>
2024-12-04 23:42:28 +0000

    remove description attribute

--- dungeon_game/event.py
+++ dungeon_game/event.py
@@ -2,7 +2,6 @@ import random
 class Event:
     def __init__(self, player):
         self.active = True
-        self.description = ""
         self.player = player
 
     def activate(self):
@@ -11,28 +10,22 @@ class Event:
 class BlankEvent(Event):
     def __init__(self, player):
         super().__init__(player)
-        self.description = "Nothing eventful happens along the way."
 
     def activate(self):
-        print(self.description)
-        return self.description
+        print("Nothing eventful happens along the way.")
 
 class TrapEvent(Event):
     def __init__(self,player):
         super().__init__(player)
-        self.description = "You walk into a trap!"
 
     def activate(self):
         damage = random.randint(1,4)
         if self.active:
             self.active = False
-            print(self.description )
-            print(f"You took {damage} damage!")
+            print(f"You walk into a trap and take {damage} damage!")
             self.player.modify_hp(-damage)
-            return self.description + f"You took {damage} damage!"
         else:
             print(f"You spot a previously encountered trap and carefully make your way around it.")
-            return "You spot a previously encountered trap and carefully make your way around it."
 
 class RestEvent(Event):
     def __init__(self,player):
@@ -43,18 +36,15 @@ class RestEvent(Event):
         damage = random.randint(1,4)
         if self.active:
             self.active = False
-            print(self.description)
-            print(f"You recover {damage} hp.")
+            print(f"You discover some leftover supplies, using them to recover {damage} hp.")
             self.player.modify_hp(damage)
-            return self.description + f"You recover {damage} hp."
         else:
-            print(f"Nothing eventful happens along the way")
-            return "Nothing eventful happens along the way."
+            print(f"You do not find any additional supplies along your path")
 
 class PuzzleEvent(Event):
     def __init__(self,player):
         super().__init__(player)
-        self.description = "You stumble across a glowing tablet with an inscription:"
+        self.description = "You stumble upon a glowing tablet with a question inscribed upon it."
 
     def new_math_question(self):
         nums =  [random.randint(11,99),random.randint(11,99),random.randint(11,99)]
@@ -79,7 +69,7 @@ class PuzzleEvent(Event):
                 self.player.modify_hp(-damage)
 
         else:
-            print("The tablet lays dormant.")
+            print("You pass by a dormant tablet.")
 
 
 
