b0c3e031db262009541b80381c556a48005478db
Justin Tan <jtjtjt703@gmail.com>
2024-12-04 21:46:42 +0000

    improved docstrings

--- dungeon_game/area.py
+++ dungeon_game/area.py
@@ -21,22 +21,33 @@ char_map = {
 }
 
 def get_symbol(exits):
-        value = 0
-        if exits[Direction.WEST]:
-            value += 1000
-        if exits[Direction.NORTH]:
-            value += 100
-        if exits[Direction.EAST]:
-            value += 10
-        if exits[Direction.SOUTH]:
-            value += 1
-        return f"{value:>04}"
+    """
+    Gets the symbol code exits for a room.
+    The code is 4 characters with the 1st, 2nd, 3rd and 4th characters represent
+    West, North, East and South exits respectively. 1 means the exit is active, 0 means it is inactive.
+
+    :param exits: A dict of directions with their active state as boolean values
+    :return: A 4 char code representing the state of the exits.
+    """
+    value = 0
+    if exits[Direction.WEST]:
+        value += 1000
+    if exits[Direction.NORTH]:
+        value += 100
+    if exits[Direction.EAST]:
+        value += 10
+    if exits[Direction.SOUTH]:
+        value += 1
+    return f"{value:>04}"
 
 
 class Area:
-    """The area class represents a single space the player move through. It has exits determining valid movement options"""
+    """The area class represents a single space in the dungeon.
+
+     Each Area records its position in the dungeon and the exits in 4 cardinal directions."""
     def __init__(self,position):
-        """Initializes the area. It is a closed room by default
+        """Initializes the area as a closed room.
+        :param position: The Position of the area within the dungeon
         :return: None
         """
         self.exits = {
@@ -49,6 +60,10 @@ class Area:
         self.col = position.col
 
     def __repr__(self):
+        """
+        Returns the str representation of the area depending on its exits.
+        :return: A character from `char_map` showing the exits.
+        """
         code = get_symbol(self.exits)
         return char_map[code]
 
