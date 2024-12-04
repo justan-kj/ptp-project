193f55ac6b14d2b61eb8d846a06f0a84f08d0b8f
Justin Tan <jtjtjt703@gmail.com>
2024-12-04 15:25:54 +0000

    added tests for get_symbol and repr

--- dungeon_game/tests/test_area.py
+++ dungeon_game/tests/test_area.py
@@ -1,5 +1,5 @@
 import unittest
-from dungeon_game.area import Area
+from dungeon_game.area import Area, get_symbol
 from dungeon_game.direction import Direction,Position
 
 class TestArea(unittest.TestCase):
@@ -19,3 +19,28 @@ class TestArea(unittest.TestCase):
             self.assertFalse(self.area.exits[key])
         self.assertEqual(self.area.row, 1)
         self.assertEqual(self.area.col, 2)
+
+    def test_get_symbol(self):
+        test_exits = {Direction.NORTH: False, Direction.SOUTH: False,
+                      Direction.EAST: False, Direction.WEST: False}
+        self.assertEqual(get_symbol(test_exits), "0000")
+        test_exits = {Direction.NORTH: True, Direction.SOUTH: True,
+                      Direction.EAST: True, Direction.WEST: True}
+        self.assertEqual(get_symbol(test_exits), "1111")
+        test_exits = {Direction.NORTH: False, Direction.SOUTH: True,
+                      Direction.EAST: True, Direction.WEST: True}
+        self.assertEqual(get_symbol(test_exits), "1011")
+        test_exits = {Direction.NORTH: True, Direction.SOUTH: False,
+                      Direction.EAST: False, Direction.WEST: True}
+        self.assertEqual(get_symbol(test_exits), "1100")
+
+    def test_repr(self):
+        self.area.exits[Direction.NORTH] = True
+        self.area.exits[Direction.EAST] = True
+        self.assertEqual(repr(self.area),"└")
+        self.area.exits[Direction.EAST] = False
+        self.assertEqual(repr(self.area), "╵")
+        self.area.exits[Direction.WEST] = True
+        self.assertEqual(repr(self.area), "┘")
+
+
