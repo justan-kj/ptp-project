4bcfa7e41f06fb1d740cc71b63ad9ec920b99001
Justin Tan <jtjtjt703@gmail.com>
2024-12-04 22:12:03 +0000

    added trap event and player parameters to Blanka nd Trap

--- dungeon_game/event.py
+++ dungeon_game/event.py
@@ -1,18 +1,30 @@
-
+import random
 class Event:
-    def __init__(self):
+    def __init__(self, player):
         self.active = True
         self.description = ""
+        self.player = player
 
     def activate(self):
         pass
 
 class BlankEvent(Event):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, player):
+        super().__init__(player)
         self.description = "Nothing eventful happens along the way."
 
     def activate(self):
         return self.description
+
+class TrapEvent(Event):
+    def __init__(self,player):
+        super().__init__(player)
+        self.description = "You walk into a trap!"
+
+    def activate(self):
+        damage = random.randint(1,4)
+        self.player.modify_hp(-damage)
+        return self.description + f"You took {damage} damage!"
+
 
 
