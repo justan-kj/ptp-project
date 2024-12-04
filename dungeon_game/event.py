09809f04416c1ecfdf8fd13661092c5672decf6c
Justin Tan <jtjtjt703@gmail.com>
2024-12-04 23:13:18 +0000

    added RestEvent that heals player

--- dungeon_game/event.py
+++ dungeon_game/event.py
@@ -24,10 +24,32 @@ class TrapEvent(Event):
 
     def activate(self):
         damage = random.randint(1,4)
-        print(self.description )
-        self.player.modify_hp(-damage)
-        print(f"You took {damage} damage!")
-        return self.description + f"You took {damage} damage!"
+        if self.active:
+            self.active = False
+            print(self.description )
+            self.player.modify_hp(-damage)
+            print(f"You took {damage} damage!")
+            return self.description + f"You took {damage} damage!"
+        else:
+            print(f"You spot a previously encountered trap and carefully make your way around it.")
+            return "You spot a previously encountered trap and carefully make your way around it."
+
+class RestEvent(Event):
+    def __init__(self,player):
+        super().__init__(player)
+        self.description = "You stumble upon some leftover supplies."
+
+    def activate(self):
+        damage = random.randint(1,4)
+        if self.active:
+            self.active = False
+            print(self.description)
+            print(f"You recover {damage} hp.")
+            self.player.modify_hp(damage)
+            return self.description + f"You recover {damage} hp."
+        else:
+            print(f"Nothing eventful happens along the way")
+            return "Nothing eventful happens along the way."
 
 
 
